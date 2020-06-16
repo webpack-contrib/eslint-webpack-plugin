34542cc925725ec87749265d7ced42d093e4206c
Lior Kummer <LKummer41@gmail.com>
2020-06-16 20:51:01 +0300

    test: add tests for dirty watch file patterns
    Add failing tests that ensure dirty file watching is matching patterns
    from the files options exactly like ESLint.
    Add folder fixture because handling folder paths requires checking
    existence of folders.
    
    Preparation for requested change in #11.

---
+++ test/dirty-file-watcher-patterns.test.js
@@ -0,0 +1,154 @@
+import { resolve } from 'path';
+
+import DirtyFileWatcher from '../src/DirtyFileWatcher';
+
+describe('dirty file watcher file patterns', () => {
+  const fixtures = resolve(__dirname, 'fixtures');
+
+  it('watches pattern "folder"', () => {
+    const watcher = new DirtyFileWatcher({
+      files: [resolve(fixtures, 'folder')],
+    });
+
+    const inFolder = resolve(fixtures, 'folder/inside.js');
+    const inNestedFolder = resolve(fixtures, 'folder/nested/inside.js');
+    const wrongExtension = resolve(fixtures, 'folder/wrong.ts');
+    const outside = resolve(fixtures, 'outside.js');
+
+    watcher.getDirtyFiles({
+      fileTimestamps: new Map([
+        [inFolder, { timestamp: 1 }],
+        [inNestedFolder, { timestamp: 1 }],
+        [wrongExtension, { timestamp: 1 }],
+        [outside, { timestamp: 1 }],
+      ]),
+    });
+    const dirtyFiles = watcher.getDirtyFiles({
+      fileTimestamps: new Map([
+        [inFolder, { timestamp: 2 }],
+        [inNestedFolder, { timestamp: 2 }],
+        [wrongExtension, { timestamp: 2 }],
+        [outside, { timestamp: 2 }],
+      ]),
+    });
+
+    expect(dirtyFiles).toHaveLength(2);
+    expect(dirtyFiles).toContain(inFolder);
+    expect(dirtyFiles).toContain(inNestedFolder);
+  });
+
+  it('watches folder pattern "folder/"', () => {
+    const watcher = new DirtyFileWatcher({
+      files: [resolve(fixtures, 'folder/')],
+    });
+
+    const inFolder = resolve(fixtures, 'folder/inside.js');
+    const inNestedFolder = resolve(fixtures, 'folder/nested/inside.js');
+    const wrongExtension = resolve(fixtures, 'folder/wrong.ts');
+    const outside = resolve(fixtures, 'outside.js');
+
+    watcher.getDirtyFiles({
+      fileTimestamps: new Map([
+        [inFolder, { timestamp: 1 }],
+        [inNestedFolder, { timestamp: 1 }],
+        [wrongExtension, { timestamp: 1 }],
+        [outside, { timestamp: 1 }],
+      ]),
+    });
+    const dirtyFiles = watcher.getDirtyFiles({
+      fileTimestamps: new Map([
+        [inFolder, { timestamp: 2 }],
+        [inNestedFolder, { timestamp: 2 }],
+        [wrongExtension, { timestamp: 2 }],
+        [outside, { timestamp: 2 }],
+      ]),
+    });
+
+    expect(dirtyFiles).toHaveLength(2);
+    expect(dirtyFiles).toContain(inFolder);
+    expect(dirtyFiles).toContain(inNestedFolder);
+  });
+
+  it('watches glob pattern "**/*"', () => {
+    const watcher = new DirtyFileWatcher({
+      files: [resolve(fixtures, '**/*')],
+    });
+
+    const inside = resolve(fixtures, 'inside.js');
+    const inNestedFolder = resolve(fixtures, 'folder/nested/inside.js');
+    const wrongExtension = resolve(fixtures, 'wrong.ts');
+
+    watcher.getDirtyFiles({
+      fileTimestamps: new Map([
+        [inside, { timestamp: 1 }],
+        [inNestedFolder, { timestamp: 1 }],
+        [wrongExtension, { timestamp: 1 }],
+      ]),
+    });
+    const dirtyFiles = watcher.getDirtyFiles({
+      fileTimestamps: new Map([
+        [inside, { timestamp: 2 }],
+        [inNestedFolder, { timestamp: 2 }],
+        [wrongExtension, { timestamp: 2 }],
+      ]),
+    });
+
+    expect(dirtyFiles).toHaveLength(3);
+    expect(dirtyFiles).toContain(inside);
+    expect(dirtyFiles).toContain(inNestedFolder);
+    // options.extensions should not be enforced on file and glob patterns.
+    expect(dirtyFiles).toContain(wrongExtension);
+  });
+
+  it('watches file pattern "good.js"', () => {
+    const file = resolve(fixtures, 'good.js');
+    const watcher = new DirtyFileWatcher({
+      files: [file],
+      extensions: ['.ts'],
+    });
+
+    watcher.getDirtyFiles({
+      fileTimestamps: new Map([[file, { timestamp: 1 }]]),
+    });
+    const dirtyFiles = watcher.getDirtyFiles({
+      fileTimestamps: new Map([[file, { timestamp: 2 }]]),
+    });
+
+    // options.extensions should not be enforced on file and glob patterns.
+    expect(dirtyFiles).toHaveLength(1);
+    expect(dirtyFiles).toContain(file);
+  });
+
+  it('uses extensions with folder patterns', () => {
+    const watcher = new DirtyFileWatcher({
+      files: [resolve(fixtures, 'folder/')],
+      extensions: ['ts', '.tsx'],
+    });
+
+    const inFolder = resolve(fixtures, 'folder/inside.tsx');
+    const inNestedFolder = resolve(fixtures, 'folder/nested/inside.ts');
+    const wrongExtension = resolve(fixtures, 'folder/wrong.js');
+    const outside = resolve(fixtures, 'outside.ts');
+
+    watcher.getDirtyFiles({
+      fileTimestamps: new Map([
+        [inFolder, { timestamp: 1 }],
+        [inNestedFolder, { timestamp: 1 }],
+        [wrongExtension, { timestamp: 1 }],
+        [outside, { timestamp: 1 }],
+      ]),
+    });
+    const dirtyFiles = watcher.getDirtyFiles({
+      fileTimestamps: new Map([
+        [inFolder, { timestamp: 2 }],
+        [inNestedFolder, { timestamp: 2 }],
+        [wrongExtension, { timestamp: 2 }],
+        [outside, { timestamp: 2 }],
+      ]),
+    });
+
+    expect(dirtyFiles).toHaveLength(2);
+    expect(dirtyFiles).toContain(inFolder);
+    expect(dirtyFiles).toContain(inNestedFolder);
+  });
+});
