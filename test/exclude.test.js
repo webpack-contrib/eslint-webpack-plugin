2a300ceb54253e5408162377eb49a5693251e4b4
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2020-11-13 07:16:24 -0300

    test: add test for exclude option

---
+++ test/exclude.test.js
@@ -0,0 +1,36 @@
+import pack from './utils/pack';
+
+describe('exclude', () => {
+  it('should exclude with globs', (done) => {
+    const compiler = pack('exclude', { exclude: ['*error*'] });
+
+    compiler.run((err, stats) => {
+      expect(err).toBeNull();
+      expect(stats.hasWarnings()).toBe(false);
+      expect(stats.hasErrors()).toBe(false);
+      done();
+    });
+  });
+
+  it('should exclude files', (done) => {
+    const compiler = pack('exclude', { exclude: ['error.js'] });
+
+    compiler.run((err, stats) => {
+      expect(err).toBeNull();
+      expect(stats.hasWarnings()).toBe(false);
+      expect(stats.hasErrors()).toBe(false);
+      done();
+    });
+  });
+
+  it('should exclude folders', (done) => {
+    const compiler = pack('exclude-folder', { exclude: ['folder'] });
+
+    compiler.run((err, stats) => {
+      expect(err).toBeNull();
+      expect(stats.hasWarnings()).toBe(false);
+      expect(stats.hasErrors()).toBe(false);
+      done();
+    });
+  });
+});
