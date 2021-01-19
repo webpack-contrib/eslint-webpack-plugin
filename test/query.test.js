2bbb0ab378035e98fe92b895db7792def757e7f7
evilebottnawi <sheo13666q@gmail.com>
2021-01-19 16:34:16 +0300

    fix: webpack@4 tests

--- test/query.test.js
+++ test/query.test.js
@@ -1,14 +1,21 @@
+import path from 'path';
+
+import webpack from 'webpack';
+
 import pack from './utils/pack';
 
 describe('query', () => {
   it('should correctly resolve file despite query path', (done) => {
+    const isWebpack5 = webpack.version.startsWith('5');
     const compiler = pack(
       'query',
       {},
       {
         resolve: {
           alias: {
-            'alias-ignore': false,
+            'alias-ignore': isWebpack5
+              ? false
+              : path.resolve(__dirname, './fixtures/good.js'),
           },
         },
       }
