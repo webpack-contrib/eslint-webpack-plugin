5ca6e1496380908fbf896ed58fdd33ae0a35f33a
Michael Liu <5413532+michaelangeliu@users.noreply.github.com>
2021-01-23 00:39:43 -0600

    test: add full-of-problems test from eslint example
    - https://eslint.org/docs/user-guide/formatters/#eslint-formatters
    - This provides a single file with a variety of errors and warnings

---
+++ test/fixtures/full-of-problems.js
@@ -0,0 +1,13 @@
+/* eslint consistent-return: "error" */
+/* eslint indent: ["warn", 4] */
+/* eslint no-else-return: "warn" */
+/* eslint semi: ["warn", "always"] */
+/* eslint space-unary-ops: "error" */
+
+function addOne(i) {
+  if (i != NaN) {
+    return i ++
+  } else {
+    return
+  }
+};
