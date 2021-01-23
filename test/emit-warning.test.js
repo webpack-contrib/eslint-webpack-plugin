a1f4838817add7117e470732a84d4c339273df47
Michael Liu <5413532+michaelangeliu@users.noreply.github.com>
2021-01-23 00:40:08 -0600

    test: add emit-error and emit-warning tests
    - replace force-emit-error and force-emit-warning tests with more
    robust tests that match the functionality of eslint-loader
    - if emitError is undefined or true, it should emit errors, otherwise
    if emitError is false, it should suppress errors
    - if emitWarning is undefined or true, it should emit warnings, otherwise
    if emitWarning is false, it should suppress warnings

---
+++ test/emit-warning.test.js
@@ -0,0 +1,58 @@
+import pack from './utils/pack';
+
+describe('emit warning', () => {
+  it('should not emit warnings if emitWarning is false', (done) => {
+    const compiler = pack('warn', { emitWarning: false });
+
+    compiler.run((err, stats) => {
+      expect(err).toBeNull();
+      expect(stats.hasWarnings()).toBe(false);
+      done();
+    });
+  });
+
+  it('should emit warnings if emitWarning is undefined', (done) => {
+    const compiler = pack('warn', {});
+
+    compiler.run((err, stats) => {
+      expect(err).toBeNull();
+      expect(stats.hasWarnings()).toBe(true);
+      done();
+    });
+  });
+
+  it('should emit warnings if emitWarning is true', (done) => {
+    const compiler = pack('warn', { emitWarning: true });
+
+    compiler.run((err, stats) => {
+      expect(err).toBeNull();
+      expect(stats.hasWarnings()).toBe(true);
+      done();
+    });
+  });
+
+  it('should emit warnings, but not warnings if emitWarning is true and emitError is false', (done) => {
+    const compiler = pack('full-of-problems', {
+      emitWarning: true,
+      emitError: false,
+    });
+
+    compiler.run((err, stats) => {
+      expect(err).toBeNull();
+      expect(stats.hasWarnings()).toBe(true);
+      expect(stats.hasErrors()).toBe(false);
+      done();
+    });
+  });
+
+  it('should emit warnings and errors if emitWarning is true and emitError is undefined', (done) => {
+    const compiler = pack('full-of-problems', { emitWarning: true });
+
+    compiler.run((err, stats) => {
+      expect(err).toBeNull();
+      expect(stats.hasWarnings()).toBe(true);
+      expect(stats.hasErrors()).toBe(true);
+      done();
+    });
+  });
+});
