f7d32219eee40d4942a09b3e8bf5323695543a55
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2022-06-22 18:02:37 -0300

    style: formatting

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
+  extends: ['@commitlint/config-conventional'],
   rules: {
     "header-max-length": [0],
     "body-max-line-length": [0],
