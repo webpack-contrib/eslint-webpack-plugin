67efb34fc2ad11dcd6593fce21059aea271dcfc7
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2022-06-22 17:55:33 -0300

    feat: add eslint prefix to CLI output for better debugging (#155)
    * feat: add eslint prefix to CLI output for better debugging
    
    * fix: message
    
    * test: remove eslint from parse message
    
    * chore: update lock file

--- src/linter.js
+++ src/linter.js
@@ -64,7 +64,8 @@ function linter(key, options, compilation) {
     }
     rawResults.push(
       lintFiles(files).catch((e) => {
-        compilation.errors.push(e);
+        // @ts-ignore
+        compilation.errors.push(new ESLintError(e.message));
         return [];
       })
     );
