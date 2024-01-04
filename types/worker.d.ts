6ac0e738a61f4f8ca4f6606153f0438de9152c78
Sachin Kumar <ersachin3112@gmail.com>
2024-01-04 09:28:39 +0530

    chore: update dependencies to latest version

--- types/worker.d.ts
+++ types/worker.d.ts
@@ -3,10 +3,12 @@ export type setupOptions = {
    * - import path of eslint
    */
   eslintPath?: string | undefined;
+  configType?: string | undefined;
   /**
    * - linter options
    */
-  eslintOptions?: ESLintOptions | undefined;
+  eslintOptions: ESLintOptions;
 };
 export type ESLint = import('eslint').ESLint;
 export type ESLintOptions = import('eslint').ESLint.Options;
+export type LintResult = import('eslint').ESLint.LintResult;
